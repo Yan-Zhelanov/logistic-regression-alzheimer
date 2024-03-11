6292f1d3ba42eaebc2040255d6709d18c883acd8
Yan-Zhelanov <yzhelanov@gmail.com>
2024-03-11 15:15:48 +0300

    Update: refactoring the reading function

--- utils/common_functions.py
+++ utils/common_functions.py
@@ -1,15 +1,12 @@
-from typing import Union
-
 import pandas as pd
 
 
-def read_dataframe_file(path_to_file: str) -> Union[pd.DataFrame, None]:
-    """Reads DataFrame file."""
+def read_dataframe_file(path_to_file: str) -> pd.DataFrame:
     if path_to_file.endswith('csv'):
         return pd.read_csv(path_to_file)
-    elif path_to_file.endswith('pickle'):
+    if path_to_file.endswith('pickle'):
         return pd.read_pickle(path_to_file)
-    elif path_to_file.endswith('parquet'):
-        return pd.read_parquet(path_to_file)
-    else:
-        raise ValueError("Unsupported file format")
+    raise ValueError(
+        "Unsupported file format. Only '.csv' and '.pickle' formats are"
+        + ' supported.',
+    )
