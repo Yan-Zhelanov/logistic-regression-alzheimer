eec35399a12a3e0a0329bdb2003de99764b4a51b
Yan-Zhelanov <yzhelanov@gmail.com>
2024-03-11 15:20:02 +0300

    Update: convert config to a class

--- config/data_config.py
+++ config/data_config.py
@@ -1,16 +1,12 @@
 import os
 
-from easydict import EasyDict
-
 from utils.enums import PreprocessingType
 
 ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
 
-data_config = EasyDict()
 
-# Path to the directory with dataset files
-data_config.path_to_data = os.path.join(ROOT_DIR, 'data', 'alzheimer')
-data_config.annot_filename = 'data_info.csv'
-
-data_config.preprocess_type = PreprocessingType.normalization  # Normalization, standardization
-data_config.preprocess_params = {'a': -1, 'b': 1}
+class Config(object):
+    PATH_TO_DATA = os.path.join(ROOT_DIR, 'dataset')
+    ANNOTATION_FILENAME = 'data_info.csv'
+    PREPROCESS_TYPE = PreprocessingType.NORMALIZATION
+    PREPROCESS_PARAMS = {'a': -1, 'b': 1}
