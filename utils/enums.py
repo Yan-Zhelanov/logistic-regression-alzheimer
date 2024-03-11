15066afd8dd2c965db38b3ecf15bafa547e63521
Yan-Zhelanov <yzhelanov@gmail.com>
2024-03-11 15:14:38 +0300

    Update: enum converted to classes for typing

--- utils/enums.py
+++ utils/enums.py
@@ -1,9 +1,30 @@
-from enum import IntEnum
+from enum import Enum
 
-SetType = IntEnum('SetType', ('train', 'validation', 'test'))
 
-WeightsInitType = IntEnum('WeightsInitType', ('normal', 'uniform'))
+class SetType(Enum):
+    """Data set type."""
 
-PreprocessingType = IntEnum('PreprocessingType', ('normalization', 'standardization'))
+    TRAIN = 1
+    VALIDATION = 2
+    TEST = 3
 
-LoggingParamType = IntEnum('LoggingParamType', ('loss', 'metric'))
+
+class WeightsInitType(Enum):
+    """Weights initialization type enum."""
+
+    NORMAL = 1
+    UNIFORM = 2
+
+
+class PreprocessingType(Enum):
+    """Preprocessing type enum."""
+
+    NORMALIZATION = 1
+    STANDARDIZATION = 2
+
+
+class LoggingParamType(Enum):
+    """Logging parameter type enum."""
+
+    LOSS = 1
+    METRIC = 2
