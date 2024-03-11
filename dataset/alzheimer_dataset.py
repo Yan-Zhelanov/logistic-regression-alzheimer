54172ab239ce6dc68bace725c5bdfba7c34cc62f
Yan-Zhelanov <yzhelanov@gmail.com>
2024-03-11 15:54:27 +0300

    Update: refactoring AlzheimerDatasetPreprocessor

--- dataset/alzheimer_dataset.py
+++ dataset/alzheimer_dataset.py
@@ -1,42 +1,53 @@
 import os
 
 import cv2
+import numpy as np
 from PIL import Image
 from skimage import io
-import numpy as np
 
+from config.data_config import DataConfig
 from utils.common_functions import read_dataframe_file
 from utils.enums import SetType
 from utils.preprocessing import ImageDataPreprocessing
 
 
-class AlzheimerDataset:
-    """A class for the Alzheimer dataset. This class reads the data and preprocesses it."""
+class AlzheimerDatasetPreprocessor(object):
+    """A class for the Alzheimer dataset.
 
-    def __init__(self, config):
+    This class reads the data and preprocesses it.
+    """
+
+    def __init__(self, data_config: DataConfig) -> None:
         """Initializes the Alzheimer dataset class instance."""
-        self.config = config
+        self._data_config = data_config
+        self._annotation = read_dataframe_file(
+            os.path.join(
+                data_config.PATH_TO_DATA, data_config.ANNOTATION_FILENAME,
+            ),
+        )
+        self._preprocessing = ImageDataPreprocessing(
+            data_config.PREPROCESS_TYPE, data_config.PREPROCESS_PARAMS,
+        )
+        self._data = {}
+        for set_type in (SetType.TRAIN, SetType.VALIDATION, SetType.TEST):
+            self._data[set_type] = self._data_preprocessing(set_type)
 
-        # Reading an annotation file that contains the image path, set_type, and target values for the entire dataset
-        self.annotation = read_dataframe_file(os.path.join(config.path_to_data, config.annot_filename))
+    def get_preprocessed_data(self, set_type: SetType):
+        """Return preprocessed data."""
+        return self._data[set_type]
 
-        # Preprocessing class initialization
-        self.preprocessing = ImageDataPreprocessing(config.preprocess_type, config.preprocess_params)
-
-        # Reads the data
-        self.data = {}
-        for set_type in SetType:
-            self.data[set_type.name] = self.data_preprocessing(set_type)
-
-    def data_preprocessing(self, set_type):
+    def _data_preprocessing(self, set_type: SetType) -> dict[str, np.ndarray]:
         """Preparing data.
 
         Args:
             set_type: data set_type from SetType
 
         Returns:
-            A dict with the following data:
-                {'features: images (numpy.ndarray), 'targets': targets (numpy.ndarray), 'paths': list of paths}
+            dict[str, np.ndarray]: A dict with the following data - {
+                'features: images (numpy.ndarray),
+                'targets': targets (numpy.ndarray),
+                'paths': list of paths,
+            }
         """
         # TODO:
         #  1) Get the rows from the self.annotation-dataframe with 'set_type' == set_type.name
@@ -56,9 +67,5 @@ class AlzheimerDataset:
         #  6) Create targets from columns 'target' (except when set_type is SetType.test):
         #       - transform to numpy.ndarray with dtype=np.int64
         #  7) Return arrays of images, targets and paths as a dict
-        images = []
-        raise NotImplementedError
-
-    def __call__(self, set_type: str):
-        """Returns preprocessed data."""
-        return self.data[set_type]
+        # images = []
+        return {}
