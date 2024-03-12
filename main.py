0c01ddb4f0fc86b4e1cf6035c59f1fd8671d0c7b
Yan-Zhelanov <yzhelanov@gmail.com>
2024-03-12 23:04:17 +0300

    Update: refactoring, typing and using new structures

--- main.py
+++ main.py
@@ -1,33 +1,36 @@
-import numpy as np
 import pandas as pd
 
-from config.data_config import data_config
-from config.experiment_config import experiment_config
-from config.params_config import params_config
-from dataset.alzheimer_dataset import AlzheimerDataset
+from config.data_config import DataConfig
+from config.experiment_config import ExperimentConfig
+from config.params_config import ParamsConfig
+from dataset.alzheimer_dataset import AlzheimerDatasetPreprocessor
 from model.logistic_regression_model import LogisticRegression
+from utils.enums import SetType
 
 
-def train():
-    dataset = AlzheimerDataset(data_config)
-    train_data = dataset('train')
-    valid_data = dataset('validation')
+def train() -> None:
+    dataset = AlzheimerDatasetPreprocessor(DataConfig())
+    train_data = dataset.get_preprocessed_data(SetType.TRAIN)
+    valid_data = dataset.get_preprocessed_data(SetType.VALIDATION)
+    model = LogisticRegression(ParamsConfig(), ExperimentConfig())
+    model.train(
+        train_data['features'],
+        train_data['targets'],
+        valid_data['features'],
+        valid_data['targets'],
+    )
 
-    model = LogisticRegression(params_config, experiment_config)
 
-    model.train(train_data['features'], train_data['targets'], valid_data['features'], valid_data['targets'])
-
-
-def predict():
-    # TODO: Set experiment_config.load_model and experiment_config.load_model_epoch
-    #       before evaluating on the test dataset
-    dataset = AlzheimerDataset(data_config)
-    test_data = dataset('test')
-
-    model = LogisticRegression(params_config, experiment_config)
-    test_predictions = model.get_model_confidence(test_data['features'])[1, :]
-
-    test_results_df = pd.DataFrame({'ID': test_data['paths'], 'prediction': test_predictions})
+def predict() -> None:
+    # TODO: Set experiment_config.load_model and
+    # experiment_config.load_model_epoch before evaluating on the test dataset
+    dataset = AlzheimerDatasetPreprocessor(DataConfig())
+    test_data = dataset.get_preprocessed_data(SetType.TEST)
+    model = LogisticRegression(ParamsConfig(), ExperimentConfig())
+    test_predictions = model._get_model_confidence(test_data['features'])[1, :]
+    test_results_df = pd.DataFrame(
+        {'ID': test_data['paths'], 'prediction': test_predictions},
+    )
     test_results_df.to_csv('test_predictions.csv', index=False)
 
 
