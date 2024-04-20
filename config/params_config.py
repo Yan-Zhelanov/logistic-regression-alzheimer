f37e4130be9e4346a8a6cb8644975f090447614c
Yan-Zhelanov <yzhelanov@gmail.com>
2024-04-20 21:03:22 +0300

    Update: bias init, num iterations, regularization, input vector dimension, and weights init kwargs

--- config/params_config.py
+++ config/params_config.py
@@ -8,15 +8,15 @@ from utils.enums import WeightsInitType
 class ParamsConfig(object):
     # Weights
     WEIGHTS_INIT_TYPE = WeightsInitType.NORMAL
-    WEIGHTS_INIT_KWARGS = {'scale': 0.01, 'mean': 0}
-    BIAS_ZEROS_INIT = True
+    WEIGHTS_INIT_KWARGS = {'scale': 0.001, 'mean': 0}
+    BIAS_ZEROS_INIT = False
     # Training params
     SEED = 0
-    NUM_ITERATIONS = 1000
+    NUM_ITERATIONS = 10000
     ITERATIONS_WITHOUT_IMPROVEMENT = 10
     LEARNING_RATE = 0.0001
-    REGULARIZATION_COEFFICIENT = 0.0001
-    INPUT_VECTOR_DIMENSION = 128 * 128
+    REGULARIZATION_COEFFICIENT = 0.001
+    INPUT_VECTOR_DIMENSION = 50
     # Other
     ACTIVATION_FUNC = 'softmax'
     NUM_CLASSES = 1 if ACTIVATION_FUNC == 'sigmoid' else 2
